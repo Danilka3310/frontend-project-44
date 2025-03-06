48da2b390ff5d74f9aedecfd9c3624bfb1793e15
Your Name <you@example.com>
2025-03-06 16:32:40 +0300

    justacommit

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,31 +1,25 @@
 import gameEngine from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
-const rule = 'What number is missing in the progression?';
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const progressionLength = 10;
-
-const getProgression = (startProgression, stepProgression) => {
-  const progressions = [];
-  for (let i = 0; i < progressionLength; i += 1) {
-    const value = startProgression + stepProgression * i;
-    progressions.push(value);
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
   }
-  return progressions;
+  let divider = 2;
+  while (divider <= (number / 2)) {
+    if (number % divider === 0) {
+      return false;
+    }
+    divider += 1;
+  }
+  return true;
 };
 
 const getQuestionAndAnswer = () => {
-  const minStep = 2;
-  const maxStep = 10;
-  const start = getRandomNumber(1, 100);
-  const step = getRandomNumber(minStep, maxStep);
-  const progression = getProgression(start, step);
-  const minIndexOfHiddenNumber = 0;
-  const maxIndexOfHiddenNumber = progressionLength - 1;
-  const indexOfHiddenNumber = getRandomNumber(minIndexOfHiddenNumber, maxIndexOfHiddenNumber);
-  const answer = String(progression[indexOfHiddenNumber]);
-  progression[indexOfHiddenNumber] = '..';
-  const question = progression.join(' ');
+  const question = getRandomNumber(0, 100);
+  const answer = isPrime(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
