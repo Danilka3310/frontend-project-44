fa1b2e3590bf150fb6be81843eb9dbe45cdbbb8f
Doroshevich Dmitriy <doroshevich.2019@bk.ru>
2025-05-13 16:44:35 +0300

    project2

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import GameUtils from '../src/gameBody.js';
+import GameUtils from '../src/utils.js';
 import progression from '../src/games/progression.js';
 
 GameUtils('What number is missing in the progression?', progression);
