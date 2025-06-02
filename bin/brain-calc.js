03ddaa0260df1bbd4e747a81624534d59798dcbe
Your Name <you@example.com>
2025-06-02 18:09:20 +0300

    blet

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-
 import GameUtils from '../src/utils.js';
-
 import calculation from '../src/games/calc.js';
 
 GameUtils('What is the result of the expression?', calculation);
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import GameUtils from '../src/utils.js';
-
 import GCD from '../src/games/gcd.js';
 
 GameUtils('Find the greatest common divisor of given numbers.', GCD);
